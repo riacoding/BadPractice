b326c520d5350f8f745658149ec77753f63ce6ad
LB <lbutton@riacoding.net>
2022-06-09 17:05:48 -0600

    adds welcome message

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3>Welcome to the site</h3>
+        <p>welcome message</p>
         <a
           className="App-link"
           href="https://reactjs.org"
