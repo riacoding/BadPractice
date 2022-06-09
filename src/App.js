ae54cbb9648eb481f26775ef47bbbd407087a869
LB <lbutton@riacoding.net>
2022-06-09 17:09:03 -0600

    changed from div to main

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3>Welcome to the site</h3>
@@ -19,7 +19,7 @@ function App() {
       </header>
       <p>marketing message</p>
       <footer>copyright {new Date().getFullYear()}</footer>
-    </div>
+    </main>
   );
 }
 
