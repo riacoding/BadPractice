9e239d24e1fb1c9e78edf9336218ef534d2b610e
LB <lbutton@riacoding.net>
2022-06-09 17:03:41 -0600

    changes site welcome

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to the site</p>
+        <h3>Welcome to the site</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
