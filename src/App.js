b4d1da9dbc986ddd3bdea13a162ee896f9b6a050
LB <lbutton@riacoding.net>
2022-06-09 17:59:41 -0600

    UI changes to homepage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <main className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to the site</p>
+        <p>Welcome to the Web</p>
         <p>a worse welcome message</p>
         <a
           className="App-link"
