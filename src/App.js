62d9fd5e8238ef3f88e3a95fe96200027d4d2c41
LB <lbutton@riacoding.net>
2022-06-09 17:12:16 -0600

    worse welcome message

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to the site</p>
+        <p>a worse welcome message</p>
         <a
           className="App-link"
           href="https://reactjs.org"
